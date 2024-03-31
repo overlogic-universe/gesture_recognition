5c95405505c590a95af68d386e7c712f72d97e93
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2024-03-31 14:11:40 +0700

    initial commit

---
+++ lib/camera_app.dart
@@ -0,0 +1,73 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:gesture_recognition/main.dart';
+
+class CameraApp extends StatefulWidget {
+  const CameraApp({super.key});
+
+  @override
+  State<CameraApp> createState() => _CameraAppState();
+}
+
+class _CameraAppState extends State<CameraApp> {
+  late CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(cameras[0], ResolutionPreset.max);
+    _initializeCameraController();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    final CameraController? cameraController = controller;
+
+    // App state changed before we got the chance to initialize.
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      return;
+    }
+
+    if (state == AppLifecycleState.inactive) {
+      cameraController.dispose();
+    } else if (state == AppLifecycleState.resumed) {
+      _initializeCameraController();
+    }
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  _initializeCameraController() {
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            // Handle access errors here.
+            break;
+          default:
+            // Handle other errors here.
+            break;
+        }
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return Scaffold(
+      body: CameraPreview(controller),
+    );
+  }
+}
---
+++ lib/camera_preview_screen.dart
@@ -0,0 +1,12 @@
+// import 'package:flutter/material.dart';
+
+// class CameraPreviewScreen extends StatelessWidget {
+//   const CameraPreviewScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return  Scaffold(
+//       body: ,
+//     );
+//   }
+// }
